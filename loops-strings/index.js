b25d82a322a3d1ca72591fd2a1730e246695f461
pprritthhvvi <prithviworkfor@gmail.com>
2026-02-14 03:33:59 +0530

    Add examples for break, continue, while, and do-while loops in index.js

--- loops-strings/index.js
+++ loops-strings/index.js
@@ -20,3 +20,83 @@ for(let i =1; i <= 5; i++) {
 for (let i=5; i>0; i--) {
     console.log(i)
 }
+
+// break
+for(let i=1; i <= 6; i++) {
+    if(i == 4) {
+        break;
+    }
+    else {
+    console.log(i);
+    }
+}
+
+// continue
+for (let i=1; i<=4; i++)
+{
+    if (i==3)
+        continue;
+    else
+        console.log(i);
+        
+}
+
+// while loop
+// same as for loop just a simple change
+let n = 1;
+while(i<=10){
+    console.log("rishi");
+    i++;
+}
+
+
+// let i =1;
+// while(i < 5) {
+//     console.log("Inside the loop");
+//     if(i == 3) {
+//         i++;
+//         continue;
+//     }
+//     else
+//         console.log(i);
+//     i++;
+// }
+
+
+// do while
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while(i<6);
+
+// strings
+
+//let name1="rishi"
+
+// let name1 = `hey prithvi 
+// i have q , is it hard to bih
+// so yes `
+
+// let name1 = new String("rishi");
+
+console.log(name1);
+
+// operation --> concatenation, substing, lenght, uppercase, lowercase, charAT, indexof
+
+// let op1 = 'English ';
+// let op2 = 'Hindi ';
+// let ans =op1+op2;
+// console. log(ans);
+
+// let op1 = 'English ';
+// let op2 = 'Hindi ';
+// let finalAns = '${op1}${op2}';
+// console. log(finalAns)
+
+// console.log(op1.toUpperCase());
+
+let sentence = "Hello \\jee \\kaise\\ \\ho \\saare";
+let words = sentence.split('\\');
+console.log(words);
+console.log(words.join(','));
