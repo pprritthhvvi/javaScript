f505e1e22703b0a352472e874019c2b1e0afae04
pprritthhvvi <162743199+pprritthhvvi@users.noreply.github.com>
2026-02-26 15:57:07 +0530

    math-object, date-object

---
+++ Common-In-Built-Objects/index.js
@@ -0,0 +1,29 @@
+// math object 
+console.log("rishi");
+console.log(Math.PI);
+console.log(Math.min(60,30,24,600,712,89));
+console.log(Math.min(60,30,24,600,712,89));
+console. log(Math.round(1.4));
+console. log(Math.ceil(1.1));
+console. log(Math.abs(-5));
+console. log(Math.random( ));
+console. log(Math.sqrt(5));
+console. log(Math.pow(2,10));
+
+// date object
+let current = new Data();
+// new date()
+console.log(current);
+
+let date = new Date('may 03 2005 1:00');
+let newData = new Date(2005, 5, 3, 5);
+
+let date = new Date();
+console.log(newDate);
+
+// newDate.
+
+console.log(newDate.getDate());
+console.log(newDate.setFullYear(2001));
+console.log(newDate)
+
